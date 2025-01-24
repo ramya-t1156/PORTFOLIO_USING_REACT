6c66df19cd75e93617244602c3b7fdd3f1d7dc4a
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-01-24 14:30:02 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,3 @@
 export default function Footer(){
-    return <div className="py-4 bg-secondary text-center text-white">&copy; RTcode 2024</div>
+    return <div className="py-4 bg-secondary text-center text-white">&copy; RTcode 2025</div>
 }
